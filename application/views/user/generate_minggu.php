19325ba67d4f5de14dc0d5e177af38aba9fe1ee1
Axver <ecimoramora@gmail.com>
2019-11-25 09:10:36 +0700

    Tanggal

--- application/views/user/generate_minggu.php
+++ application/views/user/generate_minggu.php
@@ -355,11 +355,41 @@ else
 <!--										</tr>-->
 									</table>
 
+									<?php
+									function tgl_indo($tanggal){
+										$bulan = array (
+											1 =>   'Januari',
+											'Februari',
+											'Maret',
+											'April',
+											'Mei',
+											'Juni',
+											'Juli',
+											'Agustus',
+											'September',
+											'Oktober',
+											'November',
+											'Desember'
+										);
+										$pecahkan = explode('-', $tanggal);
+
+										// variabel pecahkan 0 = tanggal
+										// variabel pecahkan 1 = bulan
+										// variabel pecahkan 2 = tahun
+
+										return $pecahkan[2] . ' ' . $bulan[ (int)$pecahkan[1] ] . ' ' . $pecahkan[0];
+									}
+									?>
+
 									<div class="row">
 										<div class="col-sm-1"></div>
 										<div class="col-sm-3"><center><b>Diperiksa Oleh</b></center></div>
 										<div class="col-sm-4"></div>
-										<div class="col-sm-3"><center><b>Dibuat Oleh</b></center></div>
+										<div class="col-sm-3"><center>
+												<b>Jambi, <?php
+
+													echo tgl_indo(date('Y-m-d'));  ?></b>
+												<br/><b>Dibuat Oleh</b></center></div>
 										<div class="col-sm-1"></div>
 									</div>
 
