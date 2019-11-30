da42dec544a71badf59127f35aae87849f70a664
Axver <ecimoramora@gmail.com>
2019-11-30 18:21:56 +0700

    Hmmm

--- application/views/user/user_pengawasan_baru.php
+++ application/views/user/user_pengawasan_baru.php
@@ -300,14 +300,56 @@ else
 	    let jenis_pekerjaan_text=$("#jenis_pekerjaan option:selected").text();
       $("#tabel_satu").append('\t\t<tr>\n' +
           '\t\t\t\t\t\t\t\t\t\t<td class="tg-cly1 pekerjaan" id="'+jenis_pekerjaan+"_pekerjaan_"+kuy+'">'+jenis_pekerjaan_text+'</td>\n' +
-          '\t\t\t\t\t\t\t\t\t\t<td class="tg-cly1 pekerja" id="'+jenis_pekerjaan+"_pekerja_"+kuy+'"></td>\n' +
+          '\t\t\t\t\t\t\t\t\t\t<td class="tg-cly1 pekerja nonActive" id="'+jenis_pekerjaan+"_pekerja_"+kuy+'"></td>\n' +
           '\t\t\t\t\t\t\t\t\t\t<td class="tg-cly1 jumlah" id="'+jenis_pekerjaan+"_jumlah_"+kuy+'"></td>\n' +
           '\t\t\t\t\t\t\t\t\t\t<td class="tg-0lax"></td>\n' +
           '\t\t\t\t\t\t\t\t\t</tr>');
 
+        //    Event Listener Untuk Jenis
+        var classname = document.getElementsByClassName("pekerja");
+
+        var myFunction = function() {
+            var attribute = this.id;
+            console.log(attribute);
+            $("#"+attribute).text("");
+            $("#"+attribute).removeClass("Active");
+            $("#"+attribute).addClass("nonActive");
+            $("#id_warnai").val(attribute);
+            $("#wPekerja").modal("show");
+        };
+
+        for (var i = 0; i < classname.length; i++) {
+            classname[i].addEventListener('click', myFunction, false);
+        }
+
       kuy++;
 	}
 </script>
+
+
+
+<!--Modal-->
+
+<!-- Modal -->
+<div class="modal fade" id="wPekerja" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+			</div>
+			<div class="modal-body">
+				<input type="text" id="id_warnai" class="form form-control" disabled>
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+				<button type="button" class="btn btn-primary">Save changes</button>
+			</div>
+		</div>
+	</div>
+</div>
 
 
 
