1a74742ff43318d80911291485edda893dbcbf08
Axver <ecimoramora@gmail.com>
2019-12-05 13:54:52 +0700

    Hmmm

--- application/views/user/user_pengawasan_view_baru.php
+++ application/views/user/user_pengawasan_view_baru.php
@@ -190,12 +190,12 @@ else
 									<div class="row">
 										<div class="col-sm-4">Periode Pengawasan</div>
 										<div class="col-sm-1">:</div>
-										<div class="col-sm-6"></div>
+										<div class="col-sm-6" id="sampai"></div>
 									</div>
 									<div class="row">
 										<div class="col-sm-4">Tanggal</div>
 										<div class="col-sm-1">:</div>
-										<div class="col-sm-6"></div>
+										<div class="col-sm-6"><?php echo $this->uri->segment("3") ?></div>
 									</div>
 									<div class="row">
 										<div class="col-sm-4">Tahun Anggaran</div>
@@ -642,6 +642,44 @@ $.ajax({
               }
           });
 }
+
+
+
+Date.prototype.getWeek = function () {
+    var target  = new Date(this.valueOf());
+    var dayNr   = (this.getDay() + 6) % 7;
+    target.setDate(target.getDate() - dayNr + 3);
+    var firstThursday = target.valueOf();
+    target.setMonth(0, 1);
+    if (target.getDay() != 4) {
+        target.setMonth(0, 1 + ((4 - target.getDay()) + 7) % 7);
+    }
+    return 1 + Math.ceil((firstThursday - target) / 604800000);
+}
+
+function getDateRangeOfWeek(weekNo){
+    var d1 = new Date();
+    numOfdaysPastSinceLastMonday = eval(d1.getDay()- 1);
+    d1.setDate(d1.getDate() - numOfdaysPastSinceLastMonday);
+    var weekNoToday = d1.getWeek();
+    var weeksInTheFuture = eval( weekNo - weekNoToday );
+    d1.setDate(d1.getDate() + eval( 7 * weeksInTheFuture ));
+    var rangeIsFrom = eval(d1.getMonth()+1) +"/" + d1.getDate() + "/" + d1.getFullYear();
+    d1.setDate(d1.getDate() + 6);
+    var rangeIsTo = eval(d1.getMonth()+1) +"/" + d1.getDate() + "/" + d1.getFullYear() ;
+    return rangeIsFrom + " to "+rangeIsTo;
+};
+
+// alert(tanggal);
+// alert(getDateRangeOfWeek(minggu));
+
+
+let rentang=getDateRangeOfWeek(minggu);
+rentang=rentang.replace(" to "," sampai ")
+// alert(rentang);
+
+$("#sampai").text(rentang);
+
 </script>
 
 
