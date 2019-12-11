22b8177bffc043c52b767c6e4cbb91a02fa15f22
Axver <ecimoramora@gmail.com>
2019-12-11 14:02:01 +0700

    Mantap

--- application/controllers/Catur_wulan_gambar.php
+++ application/controllers/Catur_wulan_gambar.php
@@ -1,12 +1,26 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Catur_wulan_gambar extends CI_Controller {
+class Catur_wulan_gambar extends CI_Controller
+{
 
 	public function index()
 	{
 		$this->load->view('user/gambar_tahap');
 	}
 
+	public function hapus_gambar()
+	{
+		$gambar = $this->input->post("image");
 
+		// Hapus
+
+
+		$this->db->query("DELETE FROM gambar_tahap WHERE gambar='$gambar'");
+
+
+
+		$target = "gambar/" . $gambar;
+		unlink($target);
+	}
 }
